39fdeab6281300298ec2eec0233f1d18f5144aab
Anurag Mathur <anuragmathur1@yahoo.com>
2016-08-08 19:22:57 +0000

    added template vhosts.erb to become virtual host config for each site inisde /etc/httpd/conf.d/<sitename>

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,2 @@
+default["apache"]["sites"]["anuragmathur11"] = { "port" => 80, "domain" => "anuragmathur11.mylabserver.com"}
+default["apache"]["sites"]["anuragmathur11b"] = { "port" => 80, "domain" => "anuragmathur11b.mylabserver.com"}
