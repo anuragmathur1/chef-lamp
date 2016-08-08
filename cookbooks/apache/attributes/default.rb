ff4cbbb9693e2274f09583d33116c4b43a57edf9
Anurag Mathur <anuragmathur1@yahoo.com>
2016-08-08 20:42:55 +0000

    Added index.html.erb templates. Made changes to recipes and attributes for the index file. Added another cookbook php to be included in apache cookbook

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -1,2 +1,2 @@
-default["apache"]["sites"]["anuragmathur11"] = { "port" => 80, "domain" => "anuragmathur11.mylabserver.com"}
-default["apache"]["sites"]["anuragmathur11b"] = { "port" => 80, "domain" => "anuragmathur11b.mylabserver.com"}
+default["apache"]["sites"]["anuragmathur11"] = { "site_title" => "Anurag's website coming soon", "port" => 80, "domain" => "anuragmathur11.mylabserver.com"}
+default["apache"]["sites"]["anuragmathur11b"] = { "site_title" => "AnuragB's website coming soon", "port" => 80, "domain" => "anuragmathur11b.mylabserver.com"}
